ed401420272abe5b285459999d969d274c72f160
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-10-25 17:03:16 -0500

    Add localization to VOTD, Thanks @Haluska77

--- src/verseOfTheDay.ts
+++ src/verseOfTheDay.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import * as cheerio from 'cheerio';
 import { getVotd } from './functions/votd';
 
-export async function getVerseOfTheDay() {
-    const data = await getVotd();
+export async function getVerseOfTheDay(lang: string = "en") {
+    const data = await getVotd(lang);
     return data;
 }
